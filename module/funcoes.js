2c821acb031248d5ab5fa69eee2f54fcac54494a
samusilva310109-a11y <samusilva310109@gmail.com>
2026-04-08 16:14:07 -0300

    funcoes.js: criadas as funções "getDadosUsuario" e "getDadosContatos"

--- module/funcoes.js
+++ module/funcoes.js
@@ -5,3 +5,75 @@
  * Versão: 1.0
  ******************************************************************************************/
 
+const contatos = require("./contatos")
+const listaContatos = contatos.contatos
+
+function getListaUsuarios(){
+    let listaUsers = {
+        "usuarios":[
+        ]
+    }
+
+    listaContatos["whats-users"].forEach((users) => {
+        listaUsers.usuarios.push(users)
+    })
+
+    if (listaUsers.usuarios.length == 0)
+        return false
+    else
+        return listaUsers
+}
+
+function getDadosUsuario(num){
+    let numero = String(num)
+
+    let dadosUser = {
+
+    }
+
+    listaContatos["whats-users"].forEach((user) => {
+        if(numero == user.number){
+            dadosUser = {
+                "nome":user.account,
+                "nick":user.nickname,
+                "foto":user["profile-image"],
+                "numero":user.number,
+                "cor_de_fundo":user.background,
+                "criacao":user["created-since"].start,
+                "encerramento":user["created-since"].end
+            }
+        }  
+    })
+
+    if(Object.keys(dadosUser).length == 0)
+        return false
+    else
+        return dadosUser
+}
+
+function getDadosContatos(num){
+    let numero = String(num)
+
+    let listaMeusContatos = []
+
+    listaContatos["whats-users"].forEach((user) => {
+        if (numero == user.number) {
+            user.contacts.forEach((contacts) => [
+                listaMeusContatos.push(
+                    {"nome":contacts.name,"foto_perfil":contacts.image,"descricao":contacts.description}
+                )
+            ])
+           
+        } 
+    })
+
+    if(listaMeusContatos.length == 0)
+        return false
+    else
+        return listaMeusContatos
+}
+
+
+
+
+
