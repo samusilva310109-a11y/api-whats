0aa14d7cd9bc7e804c99db33a37fc919648a6bdc
samusilva310109-a11y <samusilva310109@gmail.com>
2026-04-08 14:31:23 -0300

    Criação das estruturas base do projteo

---
+++ module/funcoes.js
@@ -0,0 +1,6 @@
+/******************************************************************************************
+ * Objetivo: Arquivo responsável por armazenar as funções necessárias para a criação da API 
+ * Autor: Samuel Silva Moreira Dos Santos
+ * Data início: 08/04/2026
+ * Versão: 1.0
+ ******************************************************************************************/
